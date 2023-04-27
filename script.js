25e69c5ae6e8cf6d7eddcf8c0f29f283a6c8f6d8
Robert Nguyen <robertng.thien@gmail.com>
2023-04-27 13:28:24 -0700

    Added javascript link to html and pseudocoded project

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('linked')
+ 
+// 1. We need a start button that responds to a click 
+// 2. After I click it, two things happen, timer starts, and we show a question
+// 3. show question, and we need a way to make the answers clickable, and when they click them we want to do what, checkAnswer()
+// 4. When we check the answer,
+//  - need to figure out what button was pressed to check if it's right or wrong
+//  - if they choose wrong, time go down
+//  - if they choose right,  add more points
+//  - check if we can move to the next question
+//  - if we can move to the next question, repeat step 3 
+//  - if we can't move to the next question, gameOver()
+
+//  5. Under the function game over the following will happen
+        // -Remove the question from the screen
+        // -Show the user the form so they can submit the intials
+        // -WHen they submit the form, save the initials and the score to localStorage
+        // -Render the score in the HighScores Page
+
+//FUnction to show the question
+
+
+//Function to check the answer
+
+
+
+
+// Function to start the timer is necessary
+// 
+
+
+
+
